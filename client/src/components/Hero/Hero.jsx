e2ab4c697bfd89501e0dc22547aec48cd6d7c1d0
ishikasrivas <ishikasrivas35@gmail.com>
2024-01-20 15:35:23 +0530

    Count changed

--- client/src/components/Hero/Hero.jsx
+++ client/src/components/Hero/Hero.jsx
@@ -35,21 +35,21 @@ const Hero = () => {
               <span>
                 <CountUp start={8800} end={9000} duration={4} /> <span>+</span>
               </span>
-              <span className="secondaryText">Premium Product</span>
+              <span className="secondaryText">Premium Product &nbsp;&nbsp;</span>
             </div>
 
             <div className="flexColCenter stat">
               <span>
                 <CountUp start={1950} end={2000} duration={4} /> <span>+</span>
               </span>
-              <span className="secondaryText">Happy Customer</span>
+              <span className="secondaryText"> Happy Customer &nbsp; </span>
             </div>
 
             <div className="flexColCenter stat">
               <span>
                 <CountUp end={28} /> <span>+</span>
               </span>
-              <span className="secondaryText">Awards Winning</span>
+              <span className="secondaryText">Awards Winning&nbsp;</span>
             </div>
           </div>
         </div>
